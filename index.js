eacb186941890a16e926e674485f7306c40448ea
dinaracode <dinara1205izmag@gmail.com>
2023-06-28 16:05:30 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,66 @@
+function furnitureCountdown(){
+    const furnitureDate = new Date("September 01, 2022 00:00");
+    const now = new Date();
+    const diff = furnitureDate - now;
+
+    const msInSecond = 1000;
+    const msInMinute= 1000*60;
+    const msInHour = 60*60*1000;
+    const msInDay = 24*60*60*1000;
+
+    const displayDay =  Math.floor(diff/msInDay);
+    document.querySelector(".days").textContent = displayDay;
+    console.log(displayDay);
+    
+    const displayHour = Math.floor((diff%msInDay) / msInHour);
+    document.querySelector(".hours").textContent = displayHour;
+    
+    const displayMinute = Math.floor((diff%msInHour) / msInMinute);
+    document.querySelector(".minutes").textContent = displayMinute;
+
+    const displaySecond = Math.floor((diff%msInMinute) / msInSecond);
+    document.querySelector(".seconds").textContent = displaySecond;
+
+
+    if (diff<=0){
+        document.querySelector(".days").textContent = 0;
+        document.querySelector(".hours").textContent = 0;
+        document.querySelector(".minutes").textContent = 0;
+        document.querySelector(".seconds").textContent = 0;
+        clearInterval(timerID);
+        finallyOpen();
+}
+}
+
+let timerID = setInterval(furnitureCountdown, 1000);
+
+function finallyOpen() {
+    const heading = document.querySelector("h1");
+    heading.textContent = "We are open and happy to see you!";
+    heading.classList.add("black");
+}
+
+const button = document.querySelector("#myButton");
+button.addEventListener("click", function() {
+document.querySelector("#myAudio").play();
+})
+
+
+////////////////////////////////////////////////////////////////////// slides 
+
+
+const items = document.querySelectorAll('.item');
+console.log(items);
+
+items.forEach(item => {
+    item.addEventListener('mouseover', () => {
+    removeFocus();
+    item.classList.add('selected');
+        })
+    removeFocus = () =>{
+    items.forEach(item =>{
+            item.classList.remove('selected');
+        })
+    }
+ })
+
